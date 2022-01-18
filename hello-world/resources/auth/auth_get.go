fba9b696e18468be6fa3ad6caa4ae82a3dfc8818
shinbunbun <takamuneyuto@gmail.com>
2022-01-18 22:55:06 +0900

    update

--- hello-world/resources/auth/auth_get.go
+++ hello-world/resources/auth/auth_get.go
@@ -3,26 +3,21 @@ package auth
 import (
 	"hello-world/random"
 	"hello-world/resources/config"
+	"hello-world/response"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
-func AuthGet() events.APIGatewayProxyResponse {
+func AuthGet(request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	channelId := config.GetEnv("CHANNEL_ID")
-	redirectUri := config.GetEnv("REDIRECT_URI")
+	redirectUri := config.GetRedirectUri(request)
 	stateHash, err := random.GenerateRandomString(32)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Internal Server Error: " + err.Error(),
-		}
+		return response.StatusCode500(err)
 	}
 	nonceHash, err := random.GenerateRandomString(32)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Internal Server Error: " + err.Error(),
-		}
+		return response.StatusCode500(err)
 	}
 	url := "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=" + channelId + "&redirect_uri=" + redirectUri + "&state=" + stateHash + "&scope=openid profile&nonce=" + nonceHash
 	return events.APIGatewayProxyResponse{
