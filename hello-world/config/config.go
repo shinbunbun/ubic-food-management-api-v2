363751bf8116aaad9ace0b366db1eddf989152e5
shinbunbun <takamuneyuto@gmail.com>
2022-01-19 16:06:19 +0900

    add GetRedirectUri to config

--- hello-world/config/config.go
+++ hello-world/config/config.go
@@ -2,15 +2,13 @@ package config
 
 import (
 	"os"
-
-	"github.com/aws/aws-lambda-go/events"
 )
 
 func GetEnv(key string) string {
 	return os.Getenv(key)
 }
 
-func GetRedirectUri(request events.APIGatewayProxyRequest) string {
+func GetRedirectUri() string {
 	if GetEnv("STAGE") == "local" {
 		return "http://localhost:3000/callback"
 	}
