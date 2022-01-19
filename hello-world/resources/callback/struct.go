2993a8600cd480bb8dd069352c9263dae45de957
shinbunbun <takamuneyuto@gmail.com>
2022-01-20 00:45:32 +0900

    delete auth_time

--- hello-world/resources/callback/struct.go
+++ hello-world/resources/callback/struct.go
@@ -10,15 +10,15 @@ type tokenResponse struct {
 }
 
 type payload struct {
-	Iss      string   `json:"iss"`
-	Sub      string   `json:"sub"`
-	Aud      string   `json:"aud"`
-	Exp      int      `json:"exp"`
-	Iat      int      `json:"iat"`
-	AuthTime int      `json:"auth_time"`
-	Nonce    string   `json:"nonce"`
-	Amr      []string `json:"amr,omitempty"`
-	Name     string   `json:"name"`
-	Picture  string   `json:"picture"`
-	Email    string   `json:"email,omitempty"`
+	Iss string `json:"iss"`
+	Sub string `json:"sub"`
+	Aud string `json:"aud"`
+	Exp int    `json:"exp"`
+	Iat int    `json:"iat"`
+	// AuthTime int      `json:"auth_time"`
+	Nonce   string   `json:"nonce"`
+	Amr     []string `json:"amr,omitempty"`
+	Name    string   `json:"name"`
+	Picture string   `json:"picture"`
+	Email   string   `json:"email,omitempty"`
 }
