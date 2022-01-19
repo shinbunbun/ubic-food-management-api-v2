0e9079db0b52b8e337769ac8254e9401927a2424
shinbunbun <takamuneyuto@gmail.com>
2022-01-19 18:02:51 +0900

    implement callback get

--- hello-world/resources/callback/callback_get.go
+++ hello-world/resources/callback/callback_get.go
@@ -1,1 +1,45 @@
 package callback
+
+import (
+	"encoding/json"
+	"errors"
+	"hello-world/response"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func CallbackGet(request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+
+	query := request.QueryStringParameters
+	if val, ok := query["error"]; !ok {
+		return response.StatusCode400(errors.New(val))
+	}
+
+	requestCookie := strings.Split(";", request.Headers["Cookie"])
+
+	err := checkState(query, requestCookie, request)
+	if err != nil {
+		return response.StatusCode400(err)
+	}
+
+	code := query["code"]
+
+	tokenRes, err := getAccessToken(code)
+	if err != nil {
+		return response.StatusCode500(err)
+	}
+	idToken := tokenRes.IdToken
+
+	idTokenPayload, err := verifyIdToken(requestCookie, idToken)
+	if err != nil {
+		return response.StatusCode500(err)
+	}
+
+	body, err := json.Marshal(idTokenPayload)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(body),
+	}
+}
