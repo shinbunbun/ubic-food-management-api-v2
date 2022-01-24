41f500ce9ec7209163a481adfc64027d3306c595
shinbunbun <takamuneyuto@gmail.com>
2022-01-24 14:51:14 +0900

    fix dynamo docker

--- dynamodb/docker_run.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -d --network lambda-local --name dynamolocal -p 8000:8000 amazon/dynamodb-local:latest -jar DynamoDBLocal.jar -sharedDb
--- local_invoke.sh
+++ local_invoke.sh
@@ -1,2 +1,2 @@
 make build
-sam local start-api --env-vars ./env.json --docker-network lambda-local
+sam local start-api --env-vars ./env.json --docker-network docker.internal
