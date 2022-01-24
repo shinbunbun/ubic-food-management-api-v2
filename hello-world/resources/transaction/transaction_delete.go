cf8d660d020875627e2d94f50b9acece221ad69a
shinbunbun <takamuneyuto@gmail.com>
2022-01-24 15:46:08 +0900

    implement TransactionDelete

--- hello-world/resources/transaction/transaction_delete.go
+++ hello-world/resources/transaction/transaction_delete.go
@@ -1,5 +1,30 @@
 package transaction
 
-func TransactionDelete() {
+import (
+	"hello-world/dynamodb"
+	"hello-world/response"
+	"hello-world/token"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/guregu/dynamo"
+)
+
+func TransactionDelete(request events.APIGatewayProxyRequest, idTokenPayload token.Payload) events.APIGatewayProxyResponse {
+
+	transactionId := request.PathParameters["transactionId"]
+
+	dynamodb.CreateTable()
+
+	keyed := []dynamo.Keyed{
+		dynamo.Keys{transactionId, "transaction-date"},
+		dynamo.Keys{transactionId, "transaction-food"},
+		dynamo.Keys{transactionId, "transaction-user"},
+	}
+
+	err := dynamodb.BatchDelete(keyed)
+	if err != nil {
+		return response.StatusCode500(err)
+	}
+
+	return response.StatusCode204()
 }
