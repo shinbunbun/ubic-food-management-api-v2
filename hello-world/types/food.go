358962036f91e5b2850a8fa0d4dca82c4687074a
shinbunbun <takamuneyuto@gmail.com>
2022-01-24 17:05:50 +0900

    add food Get

--- hello-world/types/food.go
+++ hello-world/types/food.go
@@ -1,9 +1,37 @@
 package types
 
+import (
+	"hello-world/dynamodb"
+)
+
 type Food struct {
 	ID       string `json:"id"`
 	ImageUrl string `json:"imageUrl"`
 	Maker    string `json:"maker"`
 	Name     string `json:"name"`
 	Stock    int    `json:"stock"`
+}
+
+func (f *Food) Get() error {
+
+	items, err := dynamodb.GetByID(f.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range items {
+		if v.DataType == "food-image" {
+			f.ImageUrl = v.Data
+		}
+		if v.DataType == "food-maker" {
+			f.Maker = v.Data
+		}
+		if v.DataType == "food-name" {
+			f.Name = v.Data
+		}
+		if v.DataType == "food-stock" {
+			f.Stock = v.IntData
+		}
+	}
+	return nil
 }
