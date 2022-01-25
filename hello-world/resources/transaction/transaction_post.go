c651b76681ca65b4ecc3a2d8aca2702219ebaf5a
shinbunbun <takamuneyuto@gmail.com>
2022-01-25 10:13:50 +0900

    implement stock update

--- hello-world/resources/transaction/transaction_post.go
+++ hello-world/resources/transaction/transaction_post.go
@@ -2,6 +2,7 @@ package transaction
 
 import (
 	"encoding/json"
+	"errors"
 	"hello-world/dynamodb"
 	"hello-world/response"
 	"hello-world/token"
@@ -40,6 +41,15 @@ func TransactionPost(request events.APIGatewayProxyRequest, idTokenPayload token
 		return response.StatusCode500(err)
 	}
 	transaction.Food = food
+
+	if food.Stock < 1 {
+		return response.StatusCode400(errors.New("food is out of stock"))
+	}
+
+	err = dynamodb.AddIntData(1, food.ID, "food-stock")
+	if err != nil {
+		return response.StatusCode500(err)
+	}
 
 	err = transaction.Put(idTokenPayload.Sub)
 	if err != nil {
