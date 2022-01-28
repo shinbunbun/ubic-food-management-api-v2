7ee4d2e4cb2aeeef623ee2e27fe5d98e05cc219b
shinbunbun <takamuneyuto@gmail.com>
2022-01-28 22:50:12 +0900

    sorry

--- ubic-food/hash/hash.go
+++ ubic-food/hash/hash.go
@@ -11,13 +11,19 @@ import (
 
 func CreateSha3_256Hash(val string) string {
 	hash := sha3.New256()
-	hash.Write([]byte(val))
+	_, err := hash.Write([]byte(val))
+	if err != nil {
+		panic(err)
+	}
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
 func CreateSha256HMAC(msg string) []byte {
 	key := os.Getenv("CHANNEL_SECRET")
 	mac := hmac.New(sha256.New, []byte(key))
-	mac.Write([]byte(msg))
+	_, err := mac.Write([]byte(msg))
+	if err != nil {
+		panic(err)
+	}
 	return mac.Sum(nil)
 }
