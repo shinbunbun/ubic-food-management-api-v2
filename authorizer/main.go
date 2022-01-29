03694bfe2719a2b36f0e7f4568f56129c04b5eb7
shinbunbun <takamuneyuto@gmail.com>
2022-01-29 17:34:09 +0900

    implement lambda authorizer

--- authorizer/main.go
+++ authorizer/main.go
@@ -5,11 +5,23 @@ import (
 	"errors"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, request events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	jwt := request.AuthorizationToken
-	if jwt == "" {
+	authZHeader := request.AuthorizationToken
+	if authZHeader == "" {
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Authorization header is empty")
 	}
+
+	idTokenPayload, err := verify(authZHeader)
+	if err != nil {
+		return generatePolicy(idTokenPayload.Sub, "Deny", request.MethodArn), nil
+	}
+
+	return generatePolicy(idTokenPayload.Sub, "Allow", request.MethodArn), nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
---
+++ authorizer/verify.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+	"ubic-food/api/token"
+)
+
+func verify(authZHeader string) (token.Payload, error) {
+	idToken := strings.Split(authZHeader, "Bearer ")[1]
+	idTokenArr := strings.Split(idToken, ".")
+	err := token.VerifySignature(idTokenArr)
+	if err != nil {
+		return token.Payload{}, err
+	}
+
+	idTokenPayload, err := token.GetIdTokenPayload(idTokenArr)
+	if err != nil {
+		return token.Payload{}, err
+	}
+
+	err = token.VerifyIssuer(idTokenPayload)
+	if err != nil {
+		return token.Payload{}, err
+	}
+
+	err = token.VerifyAud(idTokenPayload)
+	if err != nil {
+		return token.Payload{}, err
+	}
+
+	err = token.VerifyExp(idTokenPayload)
+	if err != nil {
+		return token.Payload{}, err
+	}
+
+	return idTokenPayload, nil
+}
