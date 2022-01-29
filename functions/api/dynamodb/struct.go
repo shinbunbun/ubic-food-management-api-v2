b5592a377b933a9409ce8d28eb56417e7f4507b6
shinbunbun <takamuneyuto@gmail.com>
2022-01-29 19:45:43 +0900

    delete api

--- functions/api/dynamodb/struct.go
+++
@@ -1,9 +0,0 @@
-package dynamodb
-
-type DynamoItem struct {
-	ID       string
-	DataType string
-	Data     string `dynamo:",omitempty"`
-	DataKind string
-	IntData  *int `dynamo:",omitempty"`
-}
--- functions/api/types/user.go
+++
@@ -1,7 +0,0 @@
-package types
-
-type User struct {
-	UserID       string        `json:"userId"`
-	Name         string        `json:"name"`
-	Transactions []Transaction `json:"transactions"`
-}
