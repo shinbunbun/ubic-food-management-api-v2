fd2b14bb353e0e824c154c6a0335890d443475e3
shinbunbun <takamuneyuto@gmail.com>
2022-07-19 03:12:14 +0000

    fix verify

--- functions/authorizer/verify.go
+++ functions/authorizer/verify.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"errors"
 	"strings"
+	"ubic-food/tools/keypair"
 	"ubic-food/tools/token"
 )
 
 func verify(authZHeader string) (token.Payload, error) {
 	idToken := strings.Split(authZHeader, "Bearer ")[1]
+
+	keyPair := keypair.KeyPair{}
+	claims, err := keyPair.Verify(idToken)
+	if err == nil {
+		payload, ok := claims.(token.Payload)
+		if !ok {
+			return token.Payload{}, errors.New("Invalid claims")
+		}
+		return payload, nil
+	}
+
 	idTokenArr := strings.Split(idToken, ".")
-	err := token.VerifySignature(idTokenArr)
+	err = token.VerifySignature(idTokenArr)
 	if err != nil {
 		return token.Payload{}, err
 	}
